c9a5bca59ed0fc85aff51eabda22e91083787103
BaliBalo <balibalo@pause-geek.fr>
2017-12-06 06:20:08 +0000

    2017.06

---
+++ 2017.06.js
@@ -0,0 +1,5 @@
+// Part 1
+for(M=$('*').innerText.trim().split('\t').map(e=>+e),l=M.length,a={},i=0;!a[M];i++)a[M]=i,p=M.indexOf(m=Math.max(...M)),M[p]=0,M=M.map((k,P)=>k+((P-p-1+l)%l<m%l)+m/l|0);i
+
+// Part 2
+for(M=$('*').innerText.trim().split('\t').map(e=>+e),l=M.length,a={},i=0;!a[M];i++)a[M]=i,p=M.indexOf(m=Math.max(...M)),M[p]=0,M=M.map((k,P)=>k+((P-p-1+l)%l<m%l)+m/l|0);i-a[M]
